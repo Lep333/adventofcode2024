68ef9a095034a9951dd109c25b327a18c0b78758
leo lo <leo.pirker@lopis.de>
2024-12-28 12:01:09 +0100

    Added day9

--- src/day9.py
+++ src/day9.py
@@ -5,37 +5,79 @@ def parse() -> str:
         return f.read()
 
 def part_one(input: str) -> int:
-    new_filesystem = ""
+    new_filesystem = create_file_system(input)
+    i = len(new_filesystem) - 1
+    while i >= 0:
+        if new_filesystem[i] != ".":
+            for n in range(i):
+                if new_filesystem[n] == ".":
+                    new_filesystem[n] = new_filesystem[i]
+                    new_filesystem[i] = "."
+                    break
+        i -= 1
+    
+    return get_checksum(new_filesystem)
+
+def part_two(input: str) -> int:
+    new_filesystem = create_file_system(input)
+    i = len(new_filesystem) - 1
+    start_pos = len(new_filesystem) - 1
+    while i > 0:
+        if new_filesystem[i] != ".":
+            if new_filesystem[i] != new_filesystem[start_pos]:
+                start_pos = i
+            if start_pos and new_filesystem[start_pos] != new_filesystem[i - 1]:
+                space_needed = start_pos - i + 1
+                start = space_start_index(new_filesystem, i, space_needed)
+                if start >= 0:
+                    move_file(new_filesystem, i, start, space_needed)
+                start_pos = start_pos - 1
+        i -= 1
+    
+    return get_checksum(new_filesystem)
+
+def create_file_system(input: str) -> list:
+    new_filesystem = []
 
     for id, num in enumerate(input):
-        if id % 2 == 0:
-            file_str = str(id) * int(num)   
+        if id % 2 == 0: 
+            for i in range(int(num)):
+                new_filesystem.append(int(int(id) / 2)) 
         else:
-            file_str = "." * int(num)
-        new_filesystem += file_str
-    while file_system_ordered(new_filesystem):
-        for i in range(0, len(new_filesystem), -1):
-            if new_filesystem[i] != ".":
-                for n in range(len(new_filesystem)):
-                    if new_filesystem[n] == ".":
-                        new_filesystem[n] = new_filesystem[i]
-                        new_filesystem[i] = "."
-    
+            for i in range(int(num)):
+                new_filesystem.append(".")
+    return new_filesystem
+
+def space_start_index(filesystem: list, end_index: int, space_needed: int) -> int:
+    start = 0
+    curr_space_seg = False
+    for i, el in enumerate(filesystem):
+        if i == end_index:
+            break
+        if el == "." and not curr_space_seg:
+            start = i
+            curr_space_seg = True
+        if el != ".":
+            curr_space_seg = False
+        if i - start + 1 >= space_needed and curr_space_seg:
+            return start
+    return -1
+
+def move_file(filesystem: list, start_file: int, start_space: int, length: int):
+    file_id = filesystem[start_file]
+    for i in range(length):
+        filesystem[start_space + i] = file_id
+        filesystem[start_file + i] = "."
+
+def get_checksum(filesystem: list):
     checksum = 0
-    for i, num in enumerate(new_filesystem):
+    for i, num in enumerate(filesystem):
+        if num == ".":
+            continue
         checksum += i * int(num)
     return checksum
 
-def file_system_ordered(input: str) -> bool:
-    char_found = False
-    for i in range(0, len(input), -1):
-        if input[i] != ".":
-            char_found = True
-        if input[i] == "." and char_found:
-            return False
-    return True
-
-
 if __name__ == "__main__":
     input = parse()
-    print("checksum total: ", part_one(input))
+    print("part one: ", part_one(input))
+    print("part two: ", part_two(input))
--- test/test_day9.py
+++ test/test_day9.py
@@ -6,3 +6,26 @@ class Test_Day9(unittest.TestCase):
         input = "2333133121414131402"
         result = day9.part_one(input)
         self.assertAlmostEqual(result, 1928)
+
+    def test_part_two(self):
+        input = "2333133121414131402"
+        result = day9.part_two(input)
+        self.assertAlmostEqual(result, 2858)
+
+    def test_move_file(self):
+        input = "2333133121414131402"
+        input = day9.create_file_system(input)
+        day9.move_file(input, 32, 12, 3)
+        self.assertTrue(input)
+
+    def test_space_start_index(self):
+        input = "2333133121414131402"
+        input = day9.create_file_system(input)
+        result = day9.space_start_index(input, 22, 4)
+        self.assertEqual(result, -1)
+
+    def test_space_start_index2(self):
+        input = "2333133121414131402"
+        input = day9.create_file_system(input)
+        result = day9.space_start_index(input, 5, 3)
+        self.assertEqual(result, 2)

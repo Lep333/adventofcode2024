b8420a8a8a0e4a97ef5da43c4fa82f743cce9a00
leo lo <leo.pirker@lopis.de>
2025-01-10 18:22:38 +0100

    Day12 almost complete?

--- src/day12.py
+++ src/day12.py
@@ -16,12 +16,15 @@ def part_one(input: list[str]) -> int:
     total_cost = 0
     visited_fields_current_field = []
     neighbours_directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    regions = 0
+
     for y, row in enumerate(input):
         for x, plant in enumerate(row):
             if (y, x) in visited_fields:
                 continue
             curr_area_queue.append((y, x))
             curr_plant = plant
+            regions += 1
             while curr_area_queue:
                 area = curr_area_queue.pop(0)
                 if area in visited_fields_current_field:
@@ -40,71 +43,90 @@ def part_one(input: list[str]) -> int:
             area_size = 0
             fence_length = 0
             visited_fields_current_field = []
+    print("found regions: ", regions)
+    print("no visited fields: ", len(visited_fields))
     return total_cost
 
 def part_two(input: list[str]):
-    visited_fields = []
-
-    row_count = len(input)
-    col_count = len(input[0])
-    curr_area_queue = []
-    curr_plant = ""
-    fence_length = 0
-    area_size = 0
     total_cost = 0
-    visited_fields_current_field = []
-    neighbours_directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    curr_border = []
+    visited_fields = []
+    regions = 0
+
     for y, row in enumerate(input):
-        for x, plant in enumerate(row):
-            if (y, x) in visited_fields:
+        for x, char in enumerate(row):
+            if (x, y) in visited_fields:
                 continue
-            curr_area_queue.append((y, x))
-            curr_plant = plant
-            while curr_area_queue:
-                area = curr_area_queue.pop(0)
-                if area in visited_fields_current_field:
-                    continue
-                if area[0] < 0 or area[1] < 0 or area[0] >= row_count or \
-                    area[1] >= col_count or input[area[0]][area[1]] != curr_plant:
-                    fence_length += 1
-                    curr_border.append(area)
-                else:
-                    for direction in neighbours_directions:
-                        new_area = (area[0] + direction[0], area[1] + direction[1])
-                        curr_area_queue.append(new_area)
-                    visited_fields_current_field.append(area)
-                    area_size += 1
-                    visited_fields.append(area)
-            sides = 0
-            curr_side = []
-            queue = []
-            visited_borders = []
-            sides = []
-            for border in curr_border:
-                queue = [border]
-                curr_side = [border]
-                while queue:
-                    border = queue.pop()
-                    if border in visited_borders:
-                        continue
-                    for other_border in curr_border:
-                        if border == other_border:
-                            continue
-                        if other_border in curr_side:
-                            continue
-                        for direction in neighbours_directions:
-                            if border[0] == other_border[0] + direction[0] and border[1] == other_border[1] + direction[1]:
-                                queue.append(other_border)
-                                curr_side.append(other_border)
-                                visited_borders.append(border)
-                sides.append(curr_side)
-                        
-            total_cost += area_size * fence_length
-            area_size = 0
-            fence_length = 0
-            visited_fields_current_field = []
+            fences, vis_fields = group_fences(input, (x, y))
+            visited_fields.extend(vis_fields)
+            regions += 1
+            for key, fences_ in fences.items():
+                combined_fences = []
+                for fence in fences_:
+                    one = (fence[0] -1, fence[1], fence[2]) if fence[2] == 0 else (fence[0], fence[1] -1, fence[2])
+                    two = (fence[0] +1, fence[1], fence[2]) if fence[2] == 0 else (fence[0], fence[1] +1, fence[2])
+                    for c_fence in combined_fences:
+                        if one in c_fence or two in c_fence:
+                            c_fence.append(fence)
+                            break
+                    else:
+                        combined_fences.append([fence])
+                total_cost += len(combined_fences) * len(vis_fields)
+
+    print("regions found: ", regions)
+    print("no visited fields: ", len(visited_fields))
     return total_cost
+
+def group_fences(input: list[str], start: tuple[int]) -> dict[list]:
+    # horizontal = 0
+    # vertical = 1
+    fences = dict()
+    queue = [start]
+    neighbours_directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    visited_fields = [start]
+
+    while queue:
+        x, y = queue.pop(0)
+    # for y, row in enumerate(input):
+    #     for x, char in enumerate(row):
+    #         curr_plant = input[y][x]
+        curr_plant = input[y][x]
+
+        for direction in neighbours_directions:
+            new_x = x + direction[0]
+            new_y = y + direction[1]
+            if (not out_of_bounds(input, new_x, new_y)) and input[new_y][new_x] == curr_plant and (new_x, new_y) not in visited_fields:
+                queue.append((new_x, new_y))
+                visited_fields.append((new_x, new_y))
+        
+        if not fences.get(curr_plant):
+            fences[curr_plant] = []
+
+        # top
+        if out_of_bounds(input, x, y - 1) or input[y - 1][x] != curr_plant:
+            fence_list = fences[curr_plant]
+            fence_list.append((x, y, 0))
+        # right
+        if out_of_bounds(input, x + 1, y) or input[y][x + 1] != curr_plant:
+            fence_list = fences[curr_plant]
+            fence_list.append((x + 1, y, 1))
+        # bottom
+        if out_of_bounds(input, x, y + 1) or input[y + 1][x] != curr_plant:
+            fence_list = fences[curr_plant]
+            fence_list.append((x, y + 1, 0))
+        # left
+        if out_of_bounds(input, x - 1, y) or input[y][x - 1] != curr_plant:
+            fence_list = fences[curr_plant]
+            fence_list.append((x, y, 1))
+
+    return fences, visited_fields
+
+def out_of_bounds(input: list[str], x: int, y: int) -> bool:
+    no_of_cols = len(input[0])
+    no_of_rows = len(input)
+
+    if x < 0 or x >= no_of_cols or y < 0 or y >= no_of_rows:
+        return True
+    return False
 
 if __name__ == "__main__":
     input = parse()
--- test/test_day12.py
+++ test/test_day12.py
@@ -19,3 +19,49 @@ class TestDay12(unittest.TestCase):
         input = input.split()
         result = day12.part_two(input)
         self.assertEqual(result, 80)
+
+    def test_part_two2(self):
+        input = "EEEEE\n\
+EXXXX\n\
+EEEEE\n\
+EXXXX\n\
+EEEEE"
+        input = input.split()
+        result = day12.part_two(input)
+        self.assertEqual(result, 236)
+
+    def test_part_two3(self):
+        input = "AAAAAA\n\
+AAABBA\n\
+AAABBA\n\
+ABBAAA\n\
+ABBAAA\n\
+AAAAAA"
+        input = input.split()
+        result = day12.part_two(input)
+        self.assertEqual(result, 368)
+
+    def test_part_two4(self):
+        input = "RRRRIICCFF\n\
+RRRRIICCCF\n\
+VVRRRCCFFF\n\
+VVRCCCJFFF\n\
+VVVVCJJCFE\n\
+VVIVCCJJEE\n\
+VVIIICJJEE\n\
+MIIIIIJJEE\n\
+MIIISIJEEE\n\
+MMMISSJEEE"
+        input = input.split()
+        result = day12.part_two(input)
+        self.assertEqual(result, 1206)
+
+    def test_part_two5(self):
+        input = "OOOOO\n\
+OXOXO\n\
+OOOOO\n\
+OXOXO\n\
+OOOOO"
+        input = input.split()
+        result = day12.part_two(input)
+        self.assertEqual(result, 436)

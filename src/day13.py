c42e7899af3bf8031d2985ab184d6088a3e4b812
leo lo <leo.pirker@lopis.de>
2025-01-15 19:52:16 +0100

    Completed day13

--- src/day13.py
+++ src/day13.py
@@ -1,6 +1,6 @@
 input_file_path="input/day13input.txt"
 
-def parse() -> list[tuple]:
+def parse(position_offset: int = 0) -> list[tuple]:
     text = ""
     with open(input_file_path, "r") as f:
         text = f.read()
@@ -27,42 +27,7 @@ def parse() -> list[tuple]:
             button_b = int(x), int(y)
         else:
             _, x, y = row.split()
-            prize = int(x), int(y)
-            bandits.append((button_a, button_b, prize))
-            button_a = ()
-            button_b = ()
-            prize = ()
-    return bandits
-
-def parse_part_two() -> list[tuple]:
-    text = ""
-    with open(input_file_path, "r") as f:
-        text = f.read()
-    
-    text = text.replace("X", "")
-    text = text.replace("Y", "")
-    text = text.replace("=", "")
-    text = text.replace(",", "")
-    text = text.replace("\n\n", "\n")
-    text = text.split("\n")
-
-    position_offset = 10000000000000
-    button_a = ()
-    button_b = ()
-    prize = ()
-    bandits = []
-    for i, row in enumerate(text):
-        if not row:
-            continue
-        if i % 3 == 0:
-            _, _, x, y = row.split()
-            button_a = int(x), int(y)
-        elif i % 3 == 1:
-            _, _, x, y = row.split()
-            button_b = int(x), int(y)
-        else:
-            _, x, y = row.split()
-            prize = (int(x) + position_offset, int(y) + position_offset)
+            prize = int(x) + position_offset, int(y) + position_offset
             bandits.append((button_a, button_b, prize))
             button_a = ()
             button_b = ()
@@ -70,21 +35,9 @@ def parse_part_two() -> list[tuple]:
     return bandits
 
 def part_one(bandits: list[tuple]) -> int:
-    max_button_pushes = 100
-    a_costs = 3
-    b_costs = 1
-
     total_cost = 0
     for bandit in bandits:
-        lowest_price = None
-        for a in range(max_button_pushes + 1):
-            for b in range(max_button_pushes + 1):
-                if bandit[0][0] * a + bandit[1][0] * b == bandit[2][0] and bandit[0][1] * a + bandit[1][1] * b == bandit[2][1]:
-                    price = a * a_costs + b * b_costs
-                    if not lowest_price or price < lowest_price:
-                        lowest_price = price
-        if lowest_price:
-            total_cost += lowest_price
+        total_cost += gaussian_elim(bandit)
 
     return total_cost
 
@@ -94,24 +47,33 @@ def part_two(bandits: list[tuple]) -> int:
 
     total_cost = 0
     for bandit in bandits:
-        lowest_price = None
-        a = bandit[2][0] / bandit[0][0]
-        x = bandit[2][1] / bandit[0][1]
-        while True:
-            still_to_do_x = bandit[2][0] - bandit[0][0] * a
-            still_to_do_y = bandit[2][1] - bandit[0][1] * a
-            times_b = still_to_do_x / bandit[1][0]
-            if type(times_b) == int and times_b * bandit[1][1] == still_to_do_y:
-                price = a * a_costs + times_b * b_costs
-                if not lowest_price or price < lowest_price:
-                    total_cost += lowest_price
-            a += 1
+        total_cost += gaussian_elim(bandit)
 
-    return total_cost
+    return int(total_cost)
+
+def gaussian_elim(bandit: list) -> int:
+    a_costs = 3
+    b_costs = 1
+    cost = 0
+
+    x = [bandit[0][0], bandit[1][0], bandit[2][0]]
+    y = [bandit[0][1], bandit[1][1], bandit[2][1]]
+    factor = y[0] / x[0]
+    y[0] = y[0] - x[0] * factor
+    y[1] = y[1] - x[1] * factor
+    y[2] = y[2] - x[2] * factor
+    y_sol = y[2] / y[1]
+    x_sol = (x[2] - x[1] * y_sol) / x[0]
+    x_f = abs(round(x_sol, 0) - x_sol) > 0.001
+    y_f = abs(round(y_sol, 0) - y_sol) > 0.001
+    if not (x_f or y_f):
+        cost = round(x_sol, 0) * a_costs + round(y_sol, 0) * b_costs
+
+    return cost
 
 if __name__ == "__main__":
+    position_offset = 10000000000000
     input = parse()
     print("total cost: ", part_one(input))
-    input = parse_part_two()
+    input = parse(position_offset=position_offset)
     print("total cost: ", part_two(input))
-        
--- test/test_day13.py
+++ test/test_day13.py
@@ -34,3 +34,36 @@ class TestDay13(unittest.TestCase):
         
         result = day13.part_one(bandits)
         self.assertEqual(result, 480)
+
+    def test_part_two(self):
+        text = "Button A: X+94, Y+34\nButton B: X+22, Y+67\nPrize: X=8400, Y=5400\n\nButton A: X+26, Y+66\nButton B: X+67, Y+21\nPrize: X=12748, Y=12176\n\nButton A: X+17, Y+86\nButton B: X+84, Y+37\nPrize: X=7870, Y=6450\n\nButton A: X+69, Y+23\nButton B: X+27, Y+71\nPrize: X=18641, Y=10279"
+        text = text.replace("X", "")
+        text = text.replace("Y", "")
+        text = text.replace("=", "")
+        text = text.replace(",", "")
+        text = text.replace("\n\n", "\n")
+        text = text.split("\n")
+
+        button_a = ()
+        button_b = ()
+        prize = ()
+        bandits = []
+        for i, row in enumerate(text):
+            if not row:
+                continue
+            if i % 3 == 0:
+                _, _, x, y = row.split()
+                button_a = int(x), int(y)
+            elif i % 3 == 1:
+                _, _, x, y = row.split()
+                button_b = int(x), int(y)
+            else:
+                _, x, y = row.split()
+                prize = int(x), int(y)
+                bandits.append((button_a, button_b, prize))
+                button_a = ()
+                button_b = ()
+                prize = ()
+        
+        result = day13.part_two(bandits)
+        self.assertEqual(result, 480)
